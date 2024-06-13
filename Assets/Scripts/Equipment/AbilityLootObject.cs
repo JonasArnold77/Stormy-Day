aaf0abca42db895b130e5c50c84f479a994d8982
JonasArnold77 <105543936+JonasArnold77@users.noreply.github.com>
2024-06-14 00:23:39 +0200

    Changes

--- Assets/Scripts/Equipment/AbilityLootObject.cs
+++ Assets/Scripts/Equipment/AbilityLootObject.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class AbilityLootObject : LootObject
@@ -7,9 +8,15 @@ public class AbilityLootObject : LootObject
     public AttackItem _AttackItem;
     private Animator _animator;
 
+    public AnimatorController Controller;
+
     public void Start()
     {
         _animator = GetComponentInChildren<Animator>();
+        var newAnim = Instantiate(_AttackItem._Animation);
+
+        Controller.AddMotion(_AttackItem._Animation);
+
         _animator.Play(_AttackItem._Animation.name);
     }
 
