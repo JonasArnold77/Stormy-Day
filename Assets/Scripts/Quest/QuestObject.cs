ea11dd3ea9dfe91578c899c25a135cd1b5a2412f
JonasArnold77 <105543936+JonasArnold77@users.noreply.github.com>
2024-08-01 12:08:37 +0200

    Some Improvements for map

--- Assets/Scripts/Quest/QuestObject.cs
+++ Assets/Scripts/Quest/QuestObject.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class QuestObject : MonoBehaviour
@@ -7,9 +8,12 @@ public class QuestObject : MonoBehaviour
     public List<QuestStep> QuestSteps = new List<QuestStep>();
     public int QuestStepIndex = 0;
 
+    public List<RectTransform> AllMarkers = new List<RectTransform>();
+
     private void Start()
     {
         QuestSteps[QuestStepIndex].QuestStepIsActive = true;
+        SetMarker();
     }
 
     private void Update()
@@ -19,8 +23,29 @@ public class QuestObject : MonoBehaviour
             QuestSteps[QuestStepIndex].QuestStepIsActive = false;
             QuestStepIndex++;
             QuestSteps[QuestStepIndex].QuestStepIsActive = true;
+            SetMarker();
         }
     }
 
+    public void SetMarker()
+    {
+        AllMarkers.ForEach(m => m.gameObject.SetActive(false));
+        if (QuestSteps[QuestStepIndex].QuestGiverName == "Father")
+        {
+            AllMarkers.Where(m => m.name == "Marker Farther").FirstOrDefault().gameObject.SetActive(true);
+        }
+        else if (QuestSteps[QuestStepIndex].QuestGiverName == "Daughter")
+        {
+            AllMarkers.Where(m => m.name == "Marker Ruins").FirstOrDefault().gameObject.SetActive(true);
+        }
+        else if (QuestSteps[QuestStepIndex].QuestGiverName == "Letter")
+        {
+            AllMarkers.Where(m => m.name == "Marker Cages").FirstOrDefault().gameObject.SetActive(true);
+        }
+        else if (QuestSteps[QuestStepIndex].QuestGiverName == "Lynx")
+        {
+            AllMarkers.Where(m => m.name == "Marker Dead Boy").FirstOrDefault().gameObject.SetActive(true);
+        }
+    }
 }
     
